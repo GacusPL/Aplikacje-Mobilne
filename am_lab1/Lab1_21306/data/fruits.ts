7305b04ba77b65c62e912ae9e13c7ca000ef92f0
GacusPL <kapiszponar123@gmail.com>
2026-03-04 20:49:41 +0100

    Update fruits.ts

--- am_lab1/Lab1_21306/data/fruits.ts
+++ am_lab1/Lab1_21306/data/fruits.ts
@@ -10,9 +10,9 @@ export type Fruit = {
 };
 
 export const fruitsData: Fruit[] = [
-    { id: 1, name: 'Jabłko', price: 2.5, quant: 15, image: require('@/assets/jablko.png') },
+    { id: 1, name: 'Jabłko', price: 2.5, quant: 15, image: require('@/assets/jabłko.png') },
     { id: 2, name: 'Banan', price: 4.0, quant: 8, image: require('@/assets/banan.png') },
     { id: 3, name: 'Truskawka', price: 12.0, quant: 30, image: require('@/assets/truskawka.png') },
-    { id: 4, name: 'Pomarańcza', price: 5.5, quant: 12, image: require('@/assets/pomarancza.jpg') },
-    { id: 5, name: 'Kiwi', price: 3.0, quant: 20, image: require('@/assets/kiwi.png') },
+    { id: 4, name: 'Pomarańcza', price: 5.5, quant: 12, image: require('@/assets/pomarańcza.jpg') },
+    { id: 5, name: 'Kiwi', price: 3.0, quant: 20, image: require('@/assets/kiwi.jpg') },
 ];
